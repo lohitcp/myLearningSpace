bfe75482abf32e9557d98523b729214e6becfc11
lohitcp <59756305+lohitcp@users.noreply.github.com>
2020-01-30 20:51:27 +0530

    Update simpleReverse.py

--- simpleReverse.py
+++ simpleReverse.py
@@ -1,5 +1,16 @@
 print "My first python program in many days!!"
 str = "Hello World!!"
-print "Using Simple Method"
+print "Input Sring : " +str
+print "Using Simple slice Method"
 print str[::-1]
+
 print "Using For loop Method"
+revStr = "";
+for loopVar in str:
+    revStr = loopVar + revStr;
+print revStr
+
+print "Using List method"
+revList = list(str)
+revList.reverse();
+print ''.join(revList);
